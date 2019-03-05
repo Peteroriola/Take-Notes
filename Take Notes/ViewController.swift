c067a15229febf8dfb51257ab1a864201a6d5727
Peter Oriola <peteroriola@ymail.com>
2019-03-05 10:54:05 +0100

    Initial Commit

---
+++ Take Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Take Notes
+//
+//  Created by Peter Oriola on 05/03/2019.
+//  Copyright © 2019 Peter Oriola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
